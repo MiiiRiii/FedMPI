54643905feafcd1c67ae57ab34d6e80789b5eb26
miri <zztiok@ajou.ac.kr>
2023-08-16 23:30:31 +0900

    new FashionMNIST  model

--- model_controller/CNN_FashionMNIST.py
+++ model_controller/CNN_FashionMNIST.py
@@ -1,21 +1,21 @@
 import torch
 
-class Model(torch.nn.Module):
+class Model(torch.torch.nn.Module):
     """
     #[참고] : https://team00csduck.tistory.com/232
     def __init__(self):
         super(Model, self).__init__()
-        self.activation = torch.nn.ReLU(inplace=False)
+        self.activation = torch.torch.nn.ReLU(inplace=False)
 
-        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, padding=2)
-        self.conv2 = torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.conv1 = torch.torch.nn.Conv2d(in_chatorch.nnels=1, out_chatorch.nnels=16, kernel_size=5, padding=2)
+        self.conv2 = torch.torch.nn.Conv2d(in_chatorch.nnels=16, out_chatorch.nnels=32, kernel_size=5, stride=1, padding=2)
         
-        self.maxpool1 = torch.nn.MaxPool2d(kernel_size=2)
-        self.maxpool2 = torch.nn.MaxPool2d(kernel_size=2)
+        self.maxpool1 = torch.torch.nn.MaxPool2d(kernel_size=2)
+        self.maxpool2 = torch.torch.nn.MaxPool2d(kernel_size=2)
         
-        self.flatten = torch.nn.Flatten()
+        self.flatten = torch.torch.nn.Flatten()
 
-        self.fc1 = torch.nn.Linear(in_features=32*(4*4), out_features=10, bias=False)
+        self.fc1 = torch.torch.nn.Linear(in_features=32*(4*4), out_features=10, bias=False)
 
         
     def forward(self, x):
@@ -32,24 +32,24 @@ class Model(torch.nn.Module):
 
         return x
     """
-
+    """
     # [참고] TiFL
     def __init__(self):
         super(Model, self).__init__()
-        self.activation = torch.nn.ReLU(inplace=False)
+        self.activation = torch.torch.nn.ReLU(inplace=False)
 
-        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3)
-        self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+        self.conv1 = torch.torch.nn.Conv2d(in_chatorch.nnels=1, out_chatorch.nnels=32, kernel_size=3)
+        self.conv2 = torch.torch.nn.Conv2d(in_chatorch.nnels=32, out_chatorch.nnels=64, kernel_size=3)
         
-        self.maxpool1 = torch.nn.MaxPool2d(kernel_size=2)
+        self.maxpool1 = torch.torch.nn.MaxPool2d(kernel_size=2)
 
-        self.fc1 = torch.nn.Linear(in_features=64*(12*12), out_features=128, bias=False)
-        self.fc2 = torch.nn.Linear(in_features=128, out_features=10)
+        self.fc1 = torch.torch.nn.Linear(in_features=64*(12*12), out_features=128, bias=False)
+        self.fc2 = torch.torch.nn.Linear(in_features=128, out_features=10)
 
-        self.dropout1 = torch.nn.Dropout(0.25)
-        self.dropout2 = torch.nn.Dropout(0.5)
+        self.dropout1 = torch.torch.nn.Dropout(0.25)
+        self.dropout2 = torch.torch.nn.Dropout(0.5)
 
-        self.flatten = torch.nn.Flatten()
+        self.flatten = torch.torch.nn.Flatten()
 
 
     def forward(self, x):
@@ -71,3 +71,25 @@ class Model(torch.nn.Module):
         
 
         return x
+    """
+
+    def __init__(self):
+        super(Model, self).__init__()
+        self.layer1 = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 16, kernel_size=5, padding=2),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2))
+        self.layer2 = torch.nn.Sequential(
+            torch.nn.Conv2d(16, 32, kernel_size=5, padding=2),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2))
+        self.fc = torch.nn.Linear(7*7*32, 10)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
